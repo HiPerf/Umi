525a34e68a43eb81b3fdab88150b4643d8fd17fd
gpascualg <gpascualg93@gmail.com>
2021-07-08 11:29:39 +0200

    Add ranges v3 and preliminary zip iterator

--- src/view/view.hpp
+++ src/view/view.hpp
@@ -6,12 +6,33 @@
 #include <tuple>
 
 
+template <typename C, typename It, typename... Its>
+inline constexpr void zip_i(C&& callback, It it, It end, Its&&... its)
+{
+    if (it != end)
+    {
+        callback(*it, *its...);
+        zip_i(std::forward<C>(callback), ++it, end, ++its...);
+    }
+}
+
+template <typename C, typename Range, typename... Ranges>
+inline constexpr void zip(C&& callback, Range r, Ranges&&... rs)
+{
+    zip_i(std::forward<C>(callback), begin(r), end(r), begin(rs)...);
+}
+
 template <typename... types>
 struct view
 {
     template <typename S, typename C>
-    inline constexpr void operator()(const S& scheme, C&& callback)
+    inline constexpr void operator()(S& scheme, C&& callback)
     {
-        std::apply(std::move(callback), boost::range::combine(scheme.get<types>()...));
+        zip(std::forward<C>(callback), scheme.template get<types>().range()...);
+        // for (auto combined : boost::range::combine(scheme.template get<types>().range()...))
+        // {
+        //     std::apply(callback, combined);
+        // }
     }
 };
+
