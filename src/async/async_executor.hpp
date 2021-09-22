6fed82150fc7d60d92a43e3265b9a788bfba6cc6
gpascualg <gpascualg93@gmail.com>
2021-09-22 23:25:11 +0200

    Allowing passing a callback to async_executors, wait until threads are spawned

--- src/async/async_executor.hpp
+++ src/async/async_executor.hpp
@@ -7,6 +7,8 @@
 #include <boost/fiber/future.hpp>
 #include <function2/function2.hpp>
 
+#include <mutex>
+#include <condition_variable>
 
 
 class async_executor_base
@@ -33,26 +35,42 @@ class async_executor : public async_executor_base
 public:
     async_executor(uint16_t number_of_threads, std::size_t capacity);
 
-    void worker();
+    template <typename C>
+    void start(C&& callback);
 };
 
 
 template <uint16_t tag>
 async_executor<tag>::async_executor(uint16_t number_of_threads, std::size_t capacity) :
     async_executor_base(number_of_threads, capacity)
-{
-    for (uint16_t i = 0; i < number_of_threads; ++i)
-    {
-        _workers.emplace_back(&async_executor<tag>::worker, this);
-    }
-}
+{}
 
 template <uint16_t tag>
-void async_executor<tag>::worker()
+template <typename C>
+void async_executor<tag>::start(C&& callback)
 {
-    boost::fibers::use_scheduling_algorithm<exclusive_work_stealing<tag>>(_number_of_threads, true);
+    std::mutex m;
+    std::condition_variable cv;
+    uint8_t waiting = _number_of_threads;
 
-    worker_impl();
+    for (uint16_t i = 0; i < _number_of_threads; ++i)
+    {
+        _workers.emplace_back([this, callback{ std::move(callback) }, &waiting, &m, &cv]() {
+            boost::fibers::use_scheduling_algorithm<exclusive_work_stealing<tag>>(_number_of_threads, true);
+
+            callback();
+
+            std::unique_lock<std::mutex> lk(m);
+            --waiting;
+            cv.notify_one();
+            lk.unlock();
+
+            worker_impl();
+        });
+    }
+
+    std::unique_lock<std::mutex> lk(m);
+    cv.wait(lk, [&] { return waiting == 0; });
 }
 
 template <typename F>
